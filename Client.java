07bca24f7900ac0401b3821474efed4cfaaac1eb
Ayesha <ayeshasa@umich.edu>
2018-01-27 15:34:15 -0500

    work
    haha

--- Client.java
+++ Client.java
@@ -1,56 +1,75 @@
-import java.io.*; 
+/* Me
+ * Client.java
+ */
+
+import java.io.*;
 import java.net.*;
 
-public class Client
+public class Client 
 {
-    public static final int SERVER_PORT = 7900;
+    public static final int SERVER_PORT = 6833;
 
-    public static int main(String[] cmds){
-        Socket server = null;//socket connection to the server
-        PrintStream sender = null;//object used to send messages to the server
-        BufferedReader  reader = null;//object used to read messages from server 
-        BufferedReader stdInput = null;//object used to read messages from cli
-        String userInput = null; //used to store input from the client-user
-        String serverResponse = null;//used to store messages from the server
+    public static void main(String[] commands) 
+    {
+	Socket clientSocket = null;  
+	PrintStream os = null;
+	BufferedReader is = null;
+	String userInput = null;
+	String serverInput = null;
+	BufferedReader stdInput = null;
 
-        //check for commands
-        if(cmds.length < 1){
-            System.out.println("Usage: client <Server IP Address>");
-            System.exit(1);
-        }
-        // setup connection's and enabling stream reading/writing (instantiate objects)
-        try {
-            server = new Socket(cmds[0], SERVER_PORT);
-            //object used to sen
-            sender =  new PrintStream(server.getInputStream());
-            reader = new BufferedReader(
-                new InputStreamReader(server.getInputStream())
-            );
-            stdInput = new BufferedReader(
-                new InputStreamReader(System.in)
-            );
-        } catch(UnknownHostException uhe){
-                System.err.println("Host unknown!");
-        } catch(IOException ioe){
-            System.err.println("Unable to establish IO for the connection!");
-        } catch(Exception e) {
-            System.err.println("An unexpected error has occurred! please restart the application!");
-            System.exit(1);
-        }
-        //null checks
-        if(server != null  && sender != null && reader ! = null){
-            try{
-                while((userInput != stdInput.readLine()) != null) {
-                    sender.println(userInput);
-                    serverResponse = reader.readLine();
-                    System.out.println("Server: "  + serverResponse);
-                } 
-            } catch (IOException ioe){
-                System.err.println("An io exception has occurred: " + ioe);
-            } catch (Exception e){
-                System.err.println("An unexpected error has occurred: " + e);
-            }
+	//Check the number of command line parameters
+	if (commands.length < 1)
+	{
+	    System.out.println("Usage: client <Server IP Address>");
+	    System.exit(1);
+	}
 
-        }
-    }
+	// Try to open a socket on SERVER_PORT
+	// Try to open input and output streams
+	try 
+	{
+	    clientSocket = new Socket(commands[0], SERVER_PORT);
+	    os = new PrintStream(clientSocket.getOutputStream());
+	    is = new BufferedReader (
+		new InputStreamReader(clientSocket.getInputStream()));
+	    stdInput = new BufferedReader(new InputStreamReader(System.in));
+	} 
+	catch (UnknownHostException e) 
+	{
+	    System.err.println("Don't know about host: hostname");
+	} 
+	catch (IOException e) 
+	{
+	    System.err.println("Couldn't get I/O for the connection to: hostname");
+	}
+
+	// If everything has been initialized then we want to write some data
+	// to the socket we have opened a connection to on port 25
+
+	if (clientSocket != null && os != null && is != null) 
+	{
+	    try 
+	    {
+		while ((userInput = stdInput.readLine())!= null)
+		{
+		    os.println(userInput);
+		    serverInput = is.readLine();
+		    System.out.println("Echo:" + serverInput);
+                    //code goes here hahahahaha
+		}
+
+		// close the input and output stream
+		// close the socket
+
+		os.close();
+		is.close();
+		clientSocket.close();   
+	    } 
+	    catch (IOException e) 
+	    {
+		System.err.println("IOException:  " + e);
+	    }
+	}
+    }           
 }
