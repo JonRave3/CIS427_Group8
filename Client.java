35b9e4c6e7b2a222d96db92e4b48d6c4aa74162e
JonRavelo <jon.ravelo.418@gmail.com>
2018-02-09 14:33:41 -0500

    updated client class

--- Client.java
+++ Client.java
@@ -8,103 +8,140 @@ import java.net.*;
 public class Client 
 {
     public static final int SERVER_PORT = 6833;
+	public static Socket clientSocket;
+	public static PrintStream os;
+	public static BufferedReader is, stdInput;
+	public static OutputStreamWriter osw;
+	public static BufferedWriter bw;
 
-    public static void main(String[] commands) 
+	public static void main(String[] commands) 
     {
-	Socket clientSocket = null;  
-	PrintStream os = null;
-	BufferedReader is = null;
-	String userInput = null;
-	String serverInput = null;
-	BufferedReader stdInput = null;
-        OutputStreamWriter osw = new OutputStreamWriter(os);
-        BufferedWriter bw = new BufferedWriter(osw);
+		String userInput = null;
+		String serverInput = null;
  
-	//Check the number of command line parameters
-	if (commands.length < 1)
-	{
-	    System.out.println("Usage: client <Server IP Address>");
-	    System.exit(1);
-	}
-
-	// Try to open a socket on SERVER_PORT
-	// Try to open input and output streams
-	try 
-	{
-	    clientSocket = new Socket(commands[0], SERVER_PORT);
-	    os = new PrintStream(clientSocket.getOutputStream());
-	    is = new BufferedReader (
-	new InputStreamReader(clientSocket.getInputStream()));
-	    stdInput = new BufferedReader(new InputStreamReader(System.in));
-	} 
-	catch (UnknownHostException e) 
-	{
-	    System.err.println("Don't know about host: hostname");
-	} 
-	catch (IOException e) 
-	{
-	    System.err.println("Couldn't get I/O for the connection to: hostname");
-	}
-
-	// If everything has been initialized then we want to write some data
-	// to the socket we have opened a connection to on port 25
-
-	if (clientSocket != null && os != null && is != null) 
-	{
-	    try 
-	    {
-		while ((userInput = stdInput.readLine())!= null)
+		//Check the number of command line parameters
+		if (commands.length > 0 && init(commands[0]);)
 		{
-		    os.println(userInput);
-		    serverInput = is.readLine();
-		    System.out.println("Echo:" + serverInput);
-                    
-                    if(userInput.equalsIgnoreCase("QUIT")){
-                        os.close();
-                        is.close();
-                        clientSocket.close(); 
-                    }//END QUIT
-                    
-                    if(userInput.contains("ADD")){
-                    String[] H8Life = userInput.split("\\s+");
-                    if(H8Life[1].length()<9 && H8Life[2].length()<9 && H8Life[3].length()<12){
-                    bw.write(userInput);
-                    bw.flush();
-                    }
-                    else{System.out.println("Incorrect syntax");}
-                    }// END ADD
-               
-                    if(userInput.contains("DELETE")){
-                    String[] H8Life = userInput.split("\\s+");
-                    if(H8Life[1].length()<5){
-                    bw.write(userInput);
-                    bw.flush();
-                    }
-                    else{System.out.println("Incorrect syntax");}
-                    }// END DELETE
-                        
-                    if(userInput.equalsIgnoreCase("LIST")){
-                    bw.write("LIST");
-                    bw.flush();
-                    }// END LIST
-                    
-                     if(userInput.contains("SHUTDOWN")){
-                    bw.write("SHUTDOWN");
-                    bw.flush();
-                    }// END SHUTDOWN
-                     
-                }// END WHILE
-		// close the input and output stream
-		// close the socket
+			run();
 
+		} else {
+			System.exit(1);
+		}
+	}//end of main()
+
+	private static void init(String ip) {
+		boolean status = false
+		try 
+		{
+			// Try to open a socket on SERVER_PORT
+			clientSocket = new Socket(ip, SERVER_PORT);
+			// Try to open input and output streams
+			is = new BufferedReader (new InputStreamReader(clientSocket.getInputStream()));
+			stdInput = new BufferedReader(new InputStreamReader(System.in));
+			
+			os = new PrintStream(clientSocket.getOutputStream());
+			osw = new OutputStreamWriter(os);
+			bw = new BufferedWriter(osw);
+			status = true;
+		} 
+		catch (UnknownHostException e) 
+		{
+			System.err.println("Don't know about host: hostname");
+		} 
+		catch (IOException e) 
+		{
+			System.err.println("Couldn't get I/O for the connection to: hostname");
+		}
+		return status;
+	}//end of init()
+	
+	private static void run(){
+		try {
+			while ((userInput = stdInput.readLine()) != null)
+			{
+				if(userInput.contains("ADD")) {
+					//ADD FNAME(8) LNAME(8) PHONE(12)\n
+					if(checkAdd(userInput)){
+						sendCommand(userInput);
+					} else {
+						System.out.println("Invalid Add command. Please try again.");
+					}
+				}
+		
+				else if(userInput.contains("DELETE")) {
+					if(checkDelete(userInput)){
+						sendCommand(userInput);
+					} else {
+						System.out.println("Invalid Delete command. Please try again");
+					}
+					
+				}
+					
+				else if(userInput.equalsIgnoreCase("LIST")) {
+					sendCommand(userInput);
+				}// END LIST
+				else if(userInput.equalsIgnoreCase("QUIT")) {
+					end();
+				}//END QUIT
+				else if(userInput.equalsIgnoreCase("SHUTDOWN")) {
+					sendCommand(userInput);
+				}// END SHUTDOWN
+				
+			}// END WHILE
+			end();
+		} 
+		catch (IOException e) 
+		{
+			System.err.println("IOException:  " + e);
+		}
+
+	}//end of run()
+	private static boolean sendCommand(String validCmd){
+		try {
+			bw.write(userInput);
+		} catch (Exception e){
+			System.err.println("Error: " + e);
+		}
+	}//end of sendCommand()
+	private static boolean checkAdd(String cmd){
+		boolean valid = false;
+		String[] args = cmd.split("\\s+");
+		//arg[0] == ADD
+		if(arg[0].compareToIgnoreCase("ADD"))
+		if(args[1] != null && args[2] != null &&  args[3] != null){
+			String fname = args[1];
+			String lname = args[2];
+			String phone = args[3];
+			if(fname.length() <= 8 && fname.length() > 0 &&
+				lname.length() <= 8 && lname.length() > 0 &&
+				phone.length() <= 12 && phone.length() > 0) {
+				valid = true;
+			}
+		}
+		return valid;
+	}//end of checkAdd()
+	private static boolean checkDelete(String cmd){
+		boolean valid = false;
+		String[] args = cmd.split("\\s+");
+		if(args[0].compareToIgnoreCase("DELETE"){
+			if(args[1] != null && args[1].length() == 4) {
+				try{
+					int recordId = Integer.parseInt(args[1]);
+					valid = true;
+				} catch (Exception e){
+					System.out.println("Invalid record id.");
+				}
+			}
+		}
+		return valid;
+	}//end of checkDelete()
+
+	private static void end(){
 		os.close();
 		is.close();
 		clientSocket.close();   
-	    } 
-	    catch (IOException e) 
-	    {
-		System.err.println("IOException:  " + e);
-	    }
 	}
-    }           
+	private static boolean quit(){}//end of quit()
+	private static void shutdown(){}//end of shutdown()
+	
 }
